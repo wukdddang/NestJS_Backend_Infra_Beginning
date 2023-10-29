fcde3f9be9da8aa191483e21c221a234b9b2aef1
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-10-29 13:21:59 +0900

    feature: posts REST API 추가

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,48 @@
+import { Controller, Get } from '@nestjs/common';
+import { PostsService } from './posts.service';
+
+interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+const posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 100000000,
+    commentCount: 999999,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 해린',
+    content: '노래 연습 하고 있는 해린',
+    likeCount: 100000000,
+    commentCount: 999999,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랙핑크 로제',
+    content: '종합 운동장에서 공연하는 로제',
+    likeCount: 100000000,
+    commentCount: 999999,
+  },
+];
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getPosts(): PostModel[] {
+    return posts;
+  }
+}
