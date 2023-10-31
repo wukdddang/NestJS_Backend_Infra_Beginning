8fc295cae6c765ef5921f9948ea9547704f0fbe4
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-11-01 02:49:18 +0900

    feature: users 모듈 기능 추가 - GET, POST 요청 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { UsersModel } from './entities/users.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UsersModel)
+    private readonly usersRepository: Repository<UsersModel>,
+  ) {}
+
+  async createUser(nickname: string, email: string, password: string) {
+    const user = this.usersRepository.create({
+      nickname,
+      email,
+      password,
+    });
+
+    const newUser = await this.usersRepository.save(user);
+
+    return newUser;
+  }
+
+  async getAllUsers() {
+    return await this.usersRepository.find();
+  }
+}
